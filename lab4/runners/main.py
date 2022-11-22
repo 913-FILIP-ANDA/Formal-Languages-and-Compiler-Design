08c9e4b89755b8abeb6706c90e7c021e9d7e41d9
Filip Anda <filip_anda@yahoo.com>
2022-11-22 22:57:49 +0200

    fa

--- lab4/runners/main.py
+++ lab4/runners/main.py
@@ -4,7 +4,17 @@ from Scanner import Scanner
 
 
 
+#if __name__ == '__main__':
+#           fa = FiniteAutomata("D:/Semestrul 5/compilator/lab4/inputFiles/fa1.in")
+#           fa.display_data()
+
 if __name__ == '__main__':
-            fa = FiniteAutomata("D:/Semestrul 5/compilator/lab4/inputFiles/fa.in")
-            #fa._load_data("D:/Semestrul 5/compilator/lab4/inputFiles/identifier.in")
-            fa.display_data()
+    scanner = Scanner()
+
+    try:
+        st, pif, message = scanner.scanLineByLine("D:/Semestrul 5/compilator/lab4/inputFiles/p1.txt")
+        print("st: ", st)
+        print("pif: ", pif)
+        print(message)
+    except ValueError as ve:
+        print(ve)
